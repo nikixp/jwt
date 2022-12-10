3cc43ce1143fe0889dc4bd0a4ecc14c0a1e3bbf3
nikixp.8@github.com <8804066381Nikolaihr>
2022-12-10 14:57:09 +0200

    spring boot 3 angular 15

--- src/main/java/bg/mvr/JwtApplication.java
+++ src/main/java/bg/mvr/JwtApplication.java
@@ -1,20 +1,22 @@
 package bg.mvr;
 
-import bg.mvr.controller.AuthController;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class JwtApplication implements CommandLineRunner {
+public class JwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(new ObjectMapper().writeValueAsString(new AuthController.LoginRequest("user", "password")));
+    @Bean
+    public CommandLineRunner runner() {
+        return args -> {
+            System.out.println("app runner...");
+        };
     }
+
 }
